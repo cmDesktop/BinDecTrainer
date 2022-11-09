0bdeed36cb70f1cca7f88a4f761e85390701b44e
cmDesktop <74210924+cmDesktop@users.noreply.github.com>
2022-11-09 13:17:16 +0100

    Update BinDec.py
    fixed the input error
    Now you can just insert correct numbers

--- BinDec.py
+++ BinDec.py
@@ -1,11 +1,24 @@
 import random as r
     
+def int_input(message, low, high):
+    print("")
+    try:
+        stuff = int(input(message))
+        if stuff > low and stuff < high:
+            return stuff
+        else:
+            print("number between", low+1,"and", high-1)
+            return int_input("input error\nenter a number: ",low,high)
+    except:
+        return int_input("input error\nenter a number: ",low,high)
+
+
 print("")
 print("-----BinDec Training-----\n\n1: Random \n2: Dec-Bin \n3: Bin-Dec")
 m=2
-Mode = int(input("Mode:"))
-Rounds = int(input("Rounds:"))
-Range = int(input("Range:"))
+Mode = int_input("Mode:", 0,4)
+Rounds = int_input("Rounds:",0,100)
+Range = int_input("Range:",0,8193)
 rate_r=0
 rate_d_b = 0
 rate_b_d = 0
@@ -26,12 +39,12 @@ for ro in range(0, Rounds):
         
     if m==0:
         print("Dec:", ZahlDec)
-        ZahlGuess = int(input("Bin: "))       
+        ZahlGuess = int_input("Bin: ",-1,10000000000001)
         if format(ZahlGuess, "d") == ZahlBin:
             win=True
     if m==1:
         print("Bin:", ZahlBin)
-        ZahlGuess = int(input("Dec: "))
+        ZahlGuess = int_input("Dec: ",-1,8193)
         if ZahlGuess == ZahlDec:
             win=True
         
